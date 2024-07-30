83c3088ed157fcc0f46f534a895eaae12aa63163
huseyinkarakullukcu <huseyinkarakullukcu52@gmail.com>
2024-07-30 14:17:27 +0300

    up

--- MVC/Store/StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
+++ MVC/Store/StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab304281fd8105dde9df59823ad86e4eb348fb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+885b743e4a61c31fccc9d565663a20991514df4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
